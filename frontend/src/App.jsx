08882179ba3a696dc75e15c8ab2f09024bf8a7e3
Pavan Uthsara <uthsarapavan@gmail.com>
2025-03-22 19:43:44 +0530

    add farmer dashboard page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import FarmerDashboard from './pages/FarmerPages/FarmerDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
-        
+        <FarmerDashboard />
       </div>
     </>
   )
