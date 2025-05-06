9b57f8578093650dab27317db14363821f61cfd8
Pavan Uthsara <uthsarapavan@gmail.com>
2025-05-06 19:54:45 +0530

    complaint pdf download btn

--- frontend/src/pages/BuyerPages/ComplaintHistory.jsx
+++ frontend/src/pages/BuyerPages/ComplaintHistory.jsx
@@ -304,6 +304,10 @@ const ComplaintHistory = () => {
             </tbody>
           </table>
         </div>
+
+        <div className="mt-4 flex flex-row-reverse">      
+        <button type="button" class="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-yellow-500 dark:hover:bg-yellow-600 dark:focus:ring-yellow-700">Download Complaint Summary PDF</button>
+        </div>
       </div>
     </div>
   );
